50a517be92afcd097d28b3cb96781738e51544af
shenqz <9509394@qq.com>
2020-12-26 11:34:29 +0800

    需改发布文件

--- publish.sh
+++ publish.sh
@@ -2,16 +2,13 @@
  # @Author: shen
  # @Date: 2020-12-26 11:09:35
  # @LastEditors: shen
- # @LastEditTime: 2020-12-26 11:26:39
+ # @LastEditTime: 2020-12-26 11:32:19
  # @Description: 
 ### 
 
-set -e
 old_registry=$(npm config get registry)
 npm config set registry https://registry.npmjs.org
-set +e
 whoami=$(npm whoami 2>/dev/null)
-set -e
 
 if [ -z "$whoami" ]
 then
@@ -22,7 +19,6 @@ echo "I am: $(npm whoami)"
 
 sleep 1
 echo "Begin publish..."
-npm version prerelease
 npm publish --access=public "$@"
 
 npm config set registry ${old_registry}
